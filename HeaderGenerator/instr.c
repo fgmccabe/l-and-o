8a991c8f6567f768f147f5e08e3c8ad7c7382e89
Frank McCabe <frankmccabe@mac.com>
2017-01-16 07:44:48 -0800

    M W on variable analysis

--- HeaderGenerator/instr.c
+++ HeaderGenerator/instr.c
@@ -14,6 +14,7 @@
 #define lastInstruction term = ".";
 
 static void genIns(FILE *out,char *op,opAndSpec A1,opAndSpec A2,char *cmt,char *term);
+static void dumpDisplay(FILE *out);
 
 char *prefix = "";
 
@@ -57,6 +58,8 @@ int main(int argc,char **argv)
     fprintf(out,"     iLbl(string)               -- label in code stream\n");
 
   #include "instructions.h"
+
+    dumpDisplay(out);
       
     fprintf(out, "\n}.\n");
     
@@ -134,4 +137,125 @@ static char *capitalize(char *str){
     buffer[0] = 'A'+(buffer[0]-'a');
   }
   return buffer;
+}
+
+static char *dispOpAnd(FILE *f,char *sep,opAndSpec A,int ix){
+  switch(A){
+    case nOp:                             // No operand
+      return sep;
+    case iAh:                             // input argument register in upper slot (0..255)
+    case oAh:                             // output argument register in upper slot (0..255)
+    case iAm:                             // input argument register in middle slot (0..255)
+    case oAm:                             // output argument register in middle slot (0..255)
+    case iAl:                             // input argument register in lower slot (0..255)
+    case oAl:                             // output argument register in lower slot (0..255)
+    case iLh:				/* input local, offset 0..255 */
+    case iLm:				/* input local, offset 0..255 */
+    case iLl:				/* input local, offset 0..255 */
+    case iLc:                             // input local variable offset (0..65535)
+    case oLh:				/* output local offet 0..255 */
+    case oLm:				/* output local offet 0..255 */
+    case oLl:				/* output local offet 0..255 */
+    case oLc:                             // output local variable offset  (0..65535)
+      fprintf(f,"%sss(\" \"),disp(A%d)",sep,ix);
+      return ",";
+    case iSt:                             // input at current structure pointer
+    case oSt:                             // output to current structure pointer
+      return sep;
+    case oAr:				/* Result arity in upper slot */
+    case uAr:                             // Arity in upper slot
+    case uLt:                             // small literal in upper slot (-128..127)
+    case vSz:                             // Size of local variable vector
+    case lSz:                             // Size of local variable vector
+    case cSz:             		            // Structure size
+    case Ltl:                             // 16 bit integer offset
+      fprintf(f,"%sss(\" \"),disp(A%d)",sep,ix);
+      return ",";
+    case Es:                              // escape code (0..65535)
+      fprintf(f,"%sss(\" \"),ss(A%d)",sep,ix);
+      return ",";
+    case pcr:                             // program counter relative offset (-32768..32767)
+    case pcl:                             // long pc relative offset (-0x80000000..0x7fffffff) (24bit)
+    case ltl:                             // literal number (0..65535)
+      fprintf(f,"%sss(\" \"),ss(A%d)",sep,ix);
+      return ",";
+    default:
+      printf("Problem in generating opcode type\n");
+      exit(11);
+  }
+}
+
+static char *opAndArg(FILE *f,char *sep,opAndSpec A,int ix){
+  switch(A){
+    case nOp:                             // No operand
+      return sep;
+    case iAh:                             // input argument register in upper slot (0..255)
+    case oAh:                             // output argument register in upper slot (0..255)
+    case iAm:                             // input argument register in middle slot (0..255)
+    case oAm:                             // output argument register in middle slot (0..255)
+    case iAl:                             // input argument register in lower slot (0..255)
+    case oAl:                             // output argument register in lower slot (0..255)
+    case iLh:				/* input local, offset 0..255 */
+    case iLm:				/* input local, offset 0..255 */
+    case iLl:				/* input local, offset 0..255 */
+    case iLc:                             // input local variable offset (0..65535)
+    case oLh:				/* output local offet 0..255 */
+    case oLm:				/* output local offet 0..255 */
+    case oLl:				/* output local offet 0..255 */
+    case oLc:                             // output local variable offset  (0..65535)
+      fprintf(f,"%sA%d",sep,ix);
+      return ",";
+    case iSt:                             // input at current structure pointer
+    case oSt:                             // output to current structure pointer
+      return sep;
+    case oAr:				/* Result arity in upper slot */
+    case uAr:                             // Arity in upper slot
+    case uLt:                             // small literal in upper slot (-128..127)
+    case vSz:                             // Size of local variable vector
+    case lSz:                             // Size of local variable vector
+    case cSz:             		            // Structure size
+    case Ltl:                             // 16 bit integer offset
+    case Es:                              // escape code (0..65535)
+    case pcr:                             // program counter relative offset (-32768..32767)
+    case pcl:                             // long pc relative offset (-0x80000000..0x7fffffff) (24bit)
+    case ltl:                             // literal number (0..65535)
+      fprintf(f,"%sA%d",sep,ix);
+      return ",";
+    default:
+      printf("Problem in generating opcode type\n");
+      exit(11);
+  }
+}
+
+static void genInsDisp(FILE *out,char *op,opAndSpec A1,opAndSpec A2)
+{
+  char *sep = "(";
+
+  fprintf(out,"    disp(i%s",capitalize(op));
+  sep = opAndArg(out,sep,A1,1);
+  sep = opAndArg(out,sep,A2,2);
+
+  if(strcmp(sep,"(")==0){
+    fprintf(out,") => ss(\"%s\").\n",op);
+  }else{
+    fprintf(out,"%s) => ssSeq([ss(\"%s\")",(strcmp(sep,"(")==0?"":")"),op);
+    sep = ",";
+    sep = dispOpAnd(out,sep,A1,1);
+    dispOpAnd(out,sep,A2,2);
+    fprintf(out,"]).\n");
+  }
+}
+
+#undef instruction
+#define instruction(M,O,A1,A2,cmt)  genInsDisp(out,#M,A1,A2);
+
+#undef lastInstruction
+#define lastInstruction
+
+static void dumpDisplay(FILE *out){
+  fprintf(out,"\n\n  public implementation display[instruction] <= {\n");
+
+#include "instructions.h"
+
+  fprintf(out, "  }\n");
 }
